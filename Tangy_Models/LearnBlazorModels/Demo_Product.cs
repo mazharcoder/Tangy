c695a76775fd76703949fe3434051af97f6bec0b
Mazhar Khan <mazharwhite@hotmail.com>
2023-10-16 23:18:27 +0500

    one way data binding

---
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Tangy_Models.LearnBlazorModels;
+
+public class Demo_Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsActive { get; set; }
+}
