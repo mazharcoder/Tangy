2dd1ae9b9e1a7d988a8ec91758db6be5a1713b02
Mazhar Khan <mazharwhite@hotmail.com>
2023-10-17 17:59:21 +0500

    Product List

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -7,5 +7,5 @@ public class Demo_Product
     public double Price { get; set; }
     public bool IsActive { get; set; }
 
-    public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+    public List<Demo_ProductProp> ProductProperties { get; set; }
 }
