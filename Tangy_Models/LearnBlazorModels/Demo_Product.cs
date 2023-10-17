09a722210c71b8bf56eebc35856178a4ec33997d
Mazhar Khan <mazharwhite@hotmail.com>
2023-10-17 17:08:44 +0500

    Dropdowns and data binding

--- Tangy_Models/LearnBlazorModels/Demo_Product.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Product.cs
@@ -6,4 +6,6 @@ public class Demo_Product
     public string Name { get; set; }
     public double Price { get; set; }
     public bool IsActive { get; set; }
+
+    public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
 }
