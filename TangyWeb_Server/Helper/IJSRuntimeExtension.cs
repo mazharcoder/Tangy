ca43248aee7039eccd09963a7334d0afe6aed729
Mazhar Khan <mazharwhite@hotmail.com>
2023-10-23 08:49:08 +0500

    Assignment 7

--- TangyWeb_Server/Helper/IJSRuntimeExtension.cs
+++ TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -4,13 +4,23 @@ namespace TangyWeb_Server.Helper;
 
 public static class IJSRuntimeExtension
 {
-    public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
-    {
-        await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
-    }
-    
-    public static async ValueTask ToastrFailure(this IJSRuntime jsRuntime, string message)
-    {
-        await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
-    }
+  public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+  {
+    await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+  }
+
+  public static async ValueTask ToastrFailure(this IJSRuntime jsRuntime, string message)
+  {
+    await jsRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+  }
+  public static async ValueTask SweetAlertSuccess(this IJSRuntime jsRuntime, string title, string message)
+  {
+    await jsRuntime.InvokeVoidAsync("ShowSweetAlert", title, message, "success");
+  }
+
+  public static async ValueTask SweetAlertFailure(this IJSRuntime jsRuntime, string title, string message)
+  {
+    await jsRuntime.InvokeVoidAsync("ShowSweetAlert", title, message, "error");
+  }
+
 }
--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -8,12 +8,23 @@
 //     }
 // }
 
-function ShowToastr(type, message){
+function ShowToastr(type, message) {
     if (type === 'success') {
-        toastr.success(message, 'Operation Successful', {timeout: 5000});
+        toastr.success(message, 'Operation Successful', { timeout: 5000 });
     }
 
     if (type === 'error') {
-        toastr.error(message, 'Operation Failed', {timeout: 5000});
+        toastr.error(message, 'Operation Failed', { timeout: 5000 });
     }
+}
+
+function ShowSweetAlert(title, message, type) {
+    // Type: error
+    // Type: success
+
+    Swal.fire(
+        title,
+        message,
+        type
+    );
 }
