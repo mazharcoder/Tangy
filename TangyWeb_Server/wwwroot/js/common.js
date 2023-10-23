11561f9264a651709c1325bbceff3bf89a186490
Mazhar Khan <mazharwhite@hotmail.com>
2023-10-23 08:37:14 +0500

    IJSRuntimeExtension.cs

---
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,19 @@
+﻿// window.ShowToastr = (type, message) => {
+//     if (type === 'success') {
+//         toastr.success(message, 'Operation Successful', {timeOut: 5000});
+//     }
+//
+//     if (type === 'error') {
+//         toastr.error(message, 'Operation Failed', {timeOut: 5000});
+//     }
+// }
+
+function ShowToastr(type, message){
+    if (type === 'success') {
+        toastr.success(message, 'Operation Successful', {timeout: 5000});
+    }
+
+    if (type === 'error') {
+        toastr.error(message, 'Operation Failed', {timeout: 5000});
+    }
+}
