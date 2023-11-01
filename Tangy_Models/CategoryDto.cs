f163abbdfbf44e3752b961870f93533903e602b0
Mazhar Khan <mazharwhite@hotmail.com>
2023-11-01 23:47:35 +0500

    Category Crud

--- Tangy_Models/CategoryDto.cs
+++ Tangy_Models/CategoryDto.cs
@@ -1,7 +1,11 @@
-﻿namespace Tangy_Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tangy_Models;
 
 public class CategoryDto
 {
   public int Id { get; set; }
+
+  [Required]
   public string Name { get; set; }
 }
